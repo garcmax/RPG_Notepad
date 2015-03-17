329ab7fe1cc98d38ec166f5d1507deb06cd787f0
niwamaxou <niwamaxou@gmail.com>
2015-03-17 21:55:19 +0100

    adding :
    Post It functionnality WIP
    controller ready to accept service

--- public/js/controllers/PostItCtrl.js
+++ public/js/controllers/PostItCtrl.js
@@ -1,13 +1,23 @@
 angular.module('PostItCtrl', []).controller('PostItController', ['$scope', function($scope) {
 
+
+    //to fill by GET
     $scope.postIts = [{note: "post it 1"}, {note: "post it 2 le deuxième oui sur plusieurs lignes ça donne quoi"}];
 
     $scope.addPostIt = function () {
+        //to do with POST
         $scope.postIts.push({note: $scope.newPostIt});
         $scope.newPostIt = '';
     };
 
-    $scope.removePostIt = function () {
+    $scope.removePostIt = function (postIt) {
+        console.log(postIt);
+        var idx = $scope.postIts.indexOf(postIt);
+        console.log(idx);
+        var postItToRemove = $scope.postIts[idx];
+        console.log(postItToRemove);
 
+        //to do with DELETE
+        $scope.postIts.splice(idx, 1);
     };
 }]);
